9683f6173c13e14cb112cdaf82fa900a394fa56b
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-03-19 11:38:19 +0100

    ad contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use Psr\Log\LoggerInterface;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
+class ContactController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+	private LoggerInterface $logger; 
+    
+	
+	public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)  {
+		$this->entityManager = $entityManager;
+		$this->logger = $logger;
+        
+	}
+    
+    #[Route('/Contact', name: 'Contact')]
+    public function login(Request $request): Response
+    {
+        
+    return $this->render('contact.html.twig', [
+    ]);
+}
+}
